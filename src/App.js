abe0d8fc6bad326f8b2f5d149a2a5bcbffbe724a
Kevin Loughead <kvnloughead@gmail.com>
2021-09-09 16:22:34 -0400

    remove form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function Input({ value, setValue }) {
+function Input() {
+  const [value, setValue] = React.useState('');
 
   function handleChange(e) {
     setValue(e.target.value);
   }
 
-  return (
-    <input type="text" value={value} onChange={handleChange} />
-  );
-}
-
-function Form() {
-
-  const [value, setValue] = React.useState('');
-
-  function handleSubmit(evt) {
-    evt.preventDefault()
-    alert(value)
+  function handleDisplay() {
+    alert(value);
   }
 
   function handleReset() {
@@ -26,20 +17,41 @@ function Form() {
   }
 
   return (
-    <form name='a-form' onSubmit={handleSubmit}>
-      <Input value={value} setValue={setValue} />
-      <button type='submit'>Submit</button>
-      <button type='button' onClick={handleReset}>Reset</button>
-    </form>
-  )
+    <>
+      <input type="text" value={value} onChange={handleChange} />
+      <button onClick={handleDisplay}>Display</button>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  );
 }
-
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Input />
     </div>
   );
 }
 
 export default App;
+
+// function Form() {
+
+//   const [value, setValue] = React.useState('');
+
+//   function handleSubmit(evt) {
+//     evt.preventDefault()
+//     alert(value)
+//   }
+
+//   function handleReset() {
+//     setValue('');
+//   }
+
+//   return (
+//     <form name='a-form' onSubmit={handleSubmit}>
+//       <Input value={value} setValue={setValue} />
+//       <button type='submit'>Submit</button>
+//       <button type='button' onClick={handleReset}>Reset</button>
+//     </form>
+//   )
+// }
