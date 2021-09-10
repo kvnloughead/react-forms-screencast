0e52348cefa9cc43cbee26b8c2193288a7733f6a
Kevin Loughead <kvnloughead@gmail.com>
2021-09-10 10:48:05 -0400

    update Form component stub

--- src/components/App.js
+++ src/components/App.js
@@ -12,25 +12,3 @@ function App() {
 }
 
 export default App;
-
-// function Form() {
-
-//   const [value, setValue] = React.useState('');
-
-//   function handleSubmit(evt) {
-//     evt.preventDefault()
-//     alert(value)
-//   }
-
-//   function handleReset() {
-//     setValue('');
-//   }
-
-//   return (
-//     <form name='a-form' onSubmit={handleSubmit}>
-//       <Input value={value} setValue={setValue} />
-//       <button type='submit'>Submit</button>
-//       <button type='button' onClick={handleReset}>Reset</button>
-//     </form>
-//   )
-// }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import Input from './Input';
+
 function Form() {
   return (
-    <></>
+    <form>
+      <Input />
+    </form>
   );
 }
 
 export default Form;
+
+// function Form() {
+
+//   const [value, setValue] = React.useState('');
+
+//   function handleSubmit(evt) {
+//     evt.preventDefault()
+//     alert(value)
+//   }
+
+//   function handleReset() {
+//     setValue('');
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <Input value={value} setValue={setValue} />
+//       <button type='submit'>Submit</button>
+//       <button type='button' onClick={handleReset}>Reset</button>
+//     </form>
+//   )
+// }
